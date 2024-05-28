e7bd5b5d0e7a089179372cb89eb7c421b7a92168
Eric Xiao <9758510+ericxiao251@users.noreply.github.com>
2024-05-28 14:59:34 -0400

    Add example HTTP APIs.

---
+++ src/main/java/org/exampleHttpApi/Main.java
@@ -0,0 +1,37 @@
+package org.exampleHttpApi;
+
+
+import org.apache.flink.streaming.api.datastream.AsyncDataStream;
+import org.apache.flink.streaming.api.datastream.DataStream;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<String> inputStream = env.fromElements(
+                "5"
+                ,"6"
+                ,"7"
+                ,"8"
+                ,"9"
+        );
+        inputStream.print();
+
+        // TODO: Flink retries.
+        DataStream<String> outputStream = AsyncDataStream.unorderedWait(
+                inputStream,
+                new PokemonHttpOperator(),
+                2,
+                TimeUnit.MINUTES,
+                1000
+        );
+        outputStream.print();
+
+        env.execute();
+    }
+}
---
+++ src/main/java/org/exampleHttpApi/Pokemon.java
@@ -0,0 +1,18 @@
+package org.exampleHttpApi;
+
+// Use this tool to convert JSON to POJO: https://www.jsonschema2pojo.org/
+
+public class Pokemon {
+
+    String name;
+
+    int order;
+
+    int weight;
+
+    public Pokemon(String name, int order, int weight) {
+        this.name = name;
+        this.order = order;
+        this.weight = weight;
+    }
+}
